298583552b41f6e3a6ab68254a9a08b36752c021
cvphelps1 <careyvphelps@gmail.com>
2014-02-04 21:07:54 -0800

    very basic. toggleable beacon and flock with the time left feature

---
+++ public/javascripts/beacon.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$("#beacon").click(function(e) {
+		$('.jumbotron h1').text("BOOM");
+		$(".jumbotron p").toggleClass("active");
+	});
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+	$("#submitBtn").click(updateProject);
+}
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+}
