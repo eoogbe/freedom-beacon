037391a1ca48f90544cfc1d26423549cf249af60
Evawere Ogbe <eoogbe@stanford.edu>
2014-02-04 22:03:18 -0800

    added content from index.html to views
    Just for now while we transition from jade to handlebars, we should be
    able to look at what we have.

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,16 @@
  */
 
 exports.index = function(req, res){
-  res.render('index');
+  var friends = [
+    'Carey Phelps 6m left',
+    'Kai Kuspa 21m left',
+    'Eva Ogbe 30m left',
+    'Jennifer 20m left',
+    'Nithya 6m left',
+    'Qudus 3m left',
+    'Andres 2m left',
+    'Jorge 10m left'
+  ];
+  
+  res.render('index', {'friends': friends});
 };
